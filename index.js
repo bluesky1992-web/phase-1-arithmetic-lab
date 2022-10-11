623e71a4ea4183b63c95055a9e03749462bac465
Ali ibrahim jasim <ali.ibrahim.jasim@gmail.com>
2022-10-12 01:40:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2;
+
+
+const random = Math.floor(Math.random() * 5) ;
+console.log(random)
+
+
+let num3 = 4
+let num4 = 10
+let mod = num3 % num4;
+console.log(mod)
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,20)
+console.log(max)
